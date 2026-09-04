782cb3d5c8d656667a330e8278db92a80e00509c
Anuj Mishra <am323065@gmail.com>
2026-09-04 10:42:42 +0530

    Revert "Merge team4 communication service (Module 4.3) into main"
    This reverts commit b8cc07e11a68378dbdf66e8ddf4c12067c03b78f, reversing
    changes made to 1237145c45e44d4879c5a647928b9ff9c117439a.

--- backend/src/routes/emailEvents.js
+++
@@ -1,7 +0,0 @@
-const {
-  getAllEmailEvents
-} = require('../controllers/emailEvents.controller')
-
-module.exports = async function (fastify) {
-  fastify.get('/email-events', getAllEmailEvents)
-}
--- backend/src/routes/meetings.js
+++
@@ -1,7 +0,0 @@
-const {
-  getAllMeetings
-} = require('../controllers/meetings.controller')
-
-module.exports = async function (fastify) {
-  fastify.get('/meetings', getAllMeetings)
-}
